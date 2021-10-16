89059f36dcde1c59fd0c27b05598e14699c0c9c1
zouo <2269996456@qq.com>
2021-10-16 19:40:24 +0800

    Signed-off-by: zouo <2269996456@qq.com>

--- js/route.js
+++ js/route.js
@@ -3,9 +3,16 @@
  */
 angular.module('route', [
   //注册子路由
+  'guidePage.route'
 ])
   .config(function($stateProvider, $urlRouterProvider) {
     // 当没有匹配到合适的路由之后默然跳转的url地址，还有就是项目默认启动的url地址
     // 判断是否是第一次访问，如果是跳转到引导页，如果不是跳转到主页
-
+    if(localStorage["isFirst"])
+    {
+      $urlRouterProvider.otherwise('/tab/home');
+    }
+    else {
+      $urlRouterProvider.otherwise('/guidePage');
+    }
   });
